420712e09b60068154c08eacc81d9b88d1a9d754
RussFerns <russferns@gmail.com>
2025-11-30 20:22:50 -0500

    completed project

---
+++ non-prisma.config.ts
@@ -0,0 +1,18 @@
+    // // prisma.config.ts
+    // import 'dotenv/config'; // Loads environment variables from .env files
+    // import type { PrismaConfig } from 'prisma';
+
+    // const config: PrismaConfig = {
+    //   datasource: {
+    //     // url: process.env.DATABASE_URL, // Or directly the URL string
+    //     // ... other datasource configurations like shadowDatabaseUrl, directUrl
+    //     url: process.env.POSTGRES_PRISMA_URL, // uses connection pooling
+    //     // directUrl: process.env.POSTGRES_URL_NON_POOLING // uses a direct connection
+    //   },
+    //   // ... other Prisma configurations
+    // };
+
+    // export default config;
+
+
+
---
+++ pages/api/post/index.ts
@@ -0,0 +1,29 @@
+import { getSession } from 'next-auth/react';
+import prisma from '../../../lib/prisma';
+
+// POST /api/post
+// Required fields in body: title
+// Optional fields in body: content
+export default async function handle(req, res) {
+  const { title, content } = req.body;
+
+//   Bypassing the user session since GitHub Auth is not working...
+//   const session = await getSession({ req });
+//   const result = await prisma.post.create({
+//     data: {
+//       title: title,
+//       content: content,
+//       author: { connect: { email: session?.user?.email } },
+//     },
+//   });
+
+  const result = await prisma.post.create({
+    data: {
+      title: title,
+      content: content,
+      author: { connect: { email: 'bubby@gmail.com' } },
+    },
+  });
+  
+  res.json(result);
+}
